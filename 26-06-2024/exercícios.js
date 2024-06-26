20b3020d954bcf8d270b8a0cb1bfbced9e759dc5
anaabelar <anaj.abelar@gmail.com>
2024-06-26 10:32:27 -0300

    add exercicios

---
+++ 26-06-2024/exercícios.js
@@ -0,0 +1,14 @@
+//exercício 1
+
+const tom = "Tom";
+const firstTom = "Hanks";
+const secondTom = "Cruise";
+const myFavoriteTom ="My favorite Tom is:" + " " + tom + " " + firstTom;
+
+console.log(myFavoriteTom);
+
+// exercício 2 
+
+const programmingLanguage = "JavaSript";
+const js = programmingLanguage[0] + programmingLanguage[4];
+console.log(js);
--- 26-06-2024/variavel.js
+++ 26-06-2024/variavel.js
@@ -1,11 +1,25 @@
-let mensagem = "Olá Ana!";
-let nome; //Cria uma variavel indefinido
-let disciplina = null; //Cria variavel vazia
-let texto = "Este é um exemplo"; //Cria uma variavel com um valor
+//let mensagem = "Olá Ana!";
+//let nome; //Cria uma variavel indefinido
+//let disciplina = null; //Cria variavel vazia
+//let texto = "Este é um exemplo"; //Cria uma variavel com um valor
 
 
 
 //console.log(mensagem); 
-console.log(nome);
-console.log(disciplina);
-console.log(texto);
+//console.log(nome);
+//console.log(disciplina);
+//console.log(texto);
+
+const tom = "Tom";
+const firstTom = "Hanks";
+const secondTom = "Cruise";
+const myFavoriteTom ="My favorite Tom is:" + " " + tom + " " + firstTom;
+
+console.log(myFavoriteTom);
+
+// exercício 2 
+
+const programmingLanguage = "JavaSript";
+let js = ["J", "S"];
+
+console.log(js[0] + js[1]);
