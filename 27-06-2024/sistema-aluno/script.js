fd4189bfeb5134d646f2d48aa18a9e3c79176b55
anaabelar <anaj.abelar@gmail.com>
2024-06-28 10:33:28 -0300

    Estilizando e arrumando meu sistema

--- 27-06-2024/sistema-aluno/script.js
+++ 27-06-2024/sistema-aluno/script.js
@@ -1,16 +1,31 @@
 const button = document.querySelector("button");
 const inputNota = document.querySelector("#nota");
 
+//  Criando uma função para limpar o campo
+function clearForm() {
+    inputNota.value = "";
+
+}
   // Criando uma função
   button.addEventListener("click", function(event) {
     event.preventDefault();
-    console.log(inputNota.value);
-    
-    if (inputNota.value >= 7){
-    alert("Aprovado(a) DIVO(A)!");
-   } else { (inputNota <= 6) 
-    alert("Reprovado(a) BURRO(A)!");
-   }
-  });
+    const inputNotaValue = inputNota.value
 
-   
+    // Validação de formulário
+    if ( inputNotaValue === '' ) {
+     alert("Campo obrigatório");
+     return false;
+    }
+    
+    // Lógica
+    if (inputNotaValue >= 7){
+       alert("Você foi aprovado(a)!");
+   } else { (inputNota <= 6) 
+       alert("Você foi reprovado(a)!");
+   }
+
+  // Chamando função de limpar campo
+  clearForm();
+
+});
+ 
